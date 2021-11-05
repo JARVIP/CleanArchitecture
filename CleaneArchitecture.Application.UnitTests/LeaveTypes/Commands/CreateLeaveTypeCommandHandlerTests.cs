ff87bc53647350d812601446aa539700de0c3efa
oleg gugunava <oleggugunava@gmail.com>
2021-11-05 18:50:28 +0400

    Add Unit Testing

---
+++ CleaneArchitecture.Application.UnitTests/LeaveTypes/Commands/CreateLeaveTypeCommandHandlerTests.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.DTOs.LeaveType;
+using CleanArchitecture.Application.Exceptions;
+using CleanArchitecture.Application.Features.LeaveTypes.Handlers.Commands;
+using CleanArchitecture.Application.Features.LeaveTypes.Handlers.Queries;
+using CleanArchitecture.Application.Features.LeaveTypes.Requests.Commands;
+using CleanArchitecture.Application.Features.LeaveTypes.Requests.Queries;
+using CleanArchitecture.Application.Profiles;
+using CleanArchitecture.Application.Responses;
+using CleanArchitecture.Domain;
+using CleaneArchitecture.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Application.UnitTests.LeaveTypes.Commands
+{
+    public class CreateLeaveTypeCommandHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ILeaveTypeRepository> _mockRepo;
+        private readonly CreateLeaveTypeDto _leaveTypeDto;
+        private readonly CreateLeaveTypeCommandHandler _handler;
+
+        public CreateLeaveTypeCommandHandlerTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetLeaveTypeRepository();
+            
+            var mapperConfig = new MapperConfiguration(c => 
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+            _handler = new CreateLeaveTypeCommandHandler(_mockRepo.Object, _mapper);
+
+            _leaveTypeDto = new CreateLeaveTypeDto
+            {
+                DefaultDays = 15,
+                Name = "Test DTO"
+            };
+        }
+
+        [Fact]
+        public async Task Valid_LeaveType_Added()
+        {
+
+            var result = await _handler.Handle(new CreateLeaveTypeCommand() { LeaveTypeDto = _leaveTypeDto }, CancellationToken.None);
+
+            var leaveTypes = await _mockRepo.Object.GetAll();
+
+            result.ShouldBeOfType<int>();
+
+            leaveTypes.Count.ShouldBe(4);
+        }
+
+        [Fact]
+        public async Task InValid_LeaveType_Added()
+        {
+            _leaveTypeDto.DefaultDays = -1;
+
+            ValidationException ex = await Should.ThrowAsync<ValidationException>
+            (async () =>
+                await _handler.Handle(new CreateLeaveTypeCommand() { LeaveTypeDto = _leaveTypeDto }, CancellationToken.None)
+            );
+
+            var leaveTypes = await _mockRepo.Object.GetAll();
+
+            leaveTypes.Count.ShouldBe(3);
+        }
+    }
+}
