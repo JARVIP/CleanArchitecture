362bb146f39175c74287885e773092e2324ea51d
oleg gugunava <oleggugunava@gmail.com>
2021-11-05 18:06:11 +0400

    Configure Controllers

---
+++ CleanArchitecture.Persistence/Configuration/Entities/LeaveRequestConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Persistence.Configurations.Entities
+{
+    public class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveRequest>
+    {
+        public void Configure(EntityTypeBuilder<LeaveRequest> builder)
+        {
+            
+        }
+    }
+}
